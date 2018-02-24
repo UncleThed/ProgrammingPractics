dc61e41153626bef6a5c57b1af188482e2a864fc
theodore <uncle.thed@gmail.com>
2018-02-24 21:25:37 +0700

    add all functions to lab1.cpp

--- ProgrammingPractics/lab1.cpp
+++ ProgrammingPractics/lab1.cpp
@@ -58,6 +58,109 @@ void Pointer()
 
 }
 
+void ParityOddness()
+{
+	int a;
+	cout << "\nEnter value for check on even/odd: ";
+	cin >> a;
+	if (a % 2 == 0)
+	{
+		cout << "\nValue is " << a << " even!" << endl;
+	}
+	else
+	{
+		cout << "\nValue is " << a << " odd!" << endl;
+	}
+}
+
+void Multiplicity()
+{
+	int a;
+	cout << "\nEnter value for check on even/odd: ";
+	cin >> a;
+	if (a % 5 == 0)
+	{
+		cout << "\nValue " << a << " multiples 5!" << endl;
+	}
+	else if (a % 2 == 0)
+	{
+		cout << "\nValue " << a << " multiples 2!" << endl;
+	}
+	else
+	{
+		cout << "\nValue " << a << " doesn`t multiply 5 or 2!" << endl;
+	}
+}
+
+void Precondition()
+{
+	int a = 0;
+	while (a < 5)
+	{
+		cout << "\nValue is " << a << " now";
+		a++;
+	}
+	cout << endl;
+}
+
+void Postcondition()
+{
+	int a = 0;
+	do
+	{
+		cout << "Value is " << a << " now \n";
+		a++;
+	} while (a < 5);
+}
+
+void Iterative()
+{
+	int a = 10;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "\nIteration " << i;
+	}
+	cout << endl << endl;
+	for (int i = 0; i < a; i++)
+	{
+		a--;
+		cout << "Iteration " << i << " Value is " << a << endl;
+	}
+}
+
+void EarlyCompletion()
+{
+	int a = 10;
+	for (int i = 0; i < 10; i++)
+	{
+		a--;
+		if (i >= a)
+		{
+			cout << "\nI is more or equal A! Break of cycle";
+			break;
+		}
+	}
+	cout << "\nA is " << a << endl;
+}
+
+void Converting()
+{
+	int a = 5;
+	int b = 3;
+	float z;
+
+	z = a;
+	cout << "\nValue of z is " << z;
+
+	a = (int)z;
+	cout << "\nValue of a is " << a;
+
+	z = a / b;
+	cout << "\nValue of z is " << z;
+	z = ((float)a) / b;
+	cout << "\nValue of z is " << z << endl;
+}
+
 void LaunchTask1()
 {
 	char key;						//введенный с клавиатуры символ
@@ -90,6 +193,41 @@ void LaunchTask1()
 			break;
 		case '4':
 			Pointer();
+			cout << endl;
+			system("pause");
+			break;
+		case '5':
+			ParityOddness();
+			cout << endl;
+			system("pause");
+			break;
+		case '6':
+			Multiplicity();
+			cout << endl;
+			system("pause");
+			break;
+		case '7':
+			Precondition();
+			cout << endl;
+			system("pause");
+			break;
+		case '8':
+			Postcondition();
+			cout << endl;
+			system("pause");
+			break;
+		case '9':
+			Iterative();
+			cout << endl;
+			system("pause");
+			break;
+		case 'a':
+			EarlyCompletion();
+			cout << endl;
+			system("pause");
+			break;
+		case 'b':
+			Converting();
 			cout << endl;
 			system("pause");
 			break;
